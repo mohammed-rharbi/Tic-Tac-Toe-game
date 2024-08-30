074a15cb6eb5ac75d1b0fc11c3a46071b27edf71
mohammed-rharbi <rharbi383@gmail.com>
2024-08-30 15:45:54 +0100

    fix form submtion and creation of player objects

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let welcome_title = document.createElement('h1');
 let btn_div = document.createElement('div');
 let start_button = document.createElement('button');
 
+document.body.appendChild(welcome_section);
 img.src = '/public/original.png';
 img.className = 'logo';
 welcome_title.className = 'welcome';
@@ -17,15 +18,16 @@ start_button.className = 'start_button'
 start_button.textContent = 'START';
 btn_div.appendChild(start_button);
 welcome_section.append(img , welcome_title , btn_div );
-
 start_button.onclick = ()=>{window.location.href = '/formPage.html';
 }
 
+//
 
 
-let players_form = document.getElementById('name_form');
 
-players_form.addEventListener('submit', (e)=>{
+
+
+let players_form = document.getElementById('name_form').addEventListener('submit', (e)=>{
 
     e.preventDefault();
 
@@ -43,24 +45,34 @@ players_form.addEventListener('submit', (e)=>{
 
     const player1ID = Date.now();
     let player1 = { id: player1ID , playerName: playerOne , score: 0 };  
-    localStorage.setItem('player1' , JSON.stringify(player1));
 
 
     const player2ID = Date.now() + 1;
     let player2 = { id: player2ID, playerName: playerTwo, score: 0 };  
-    localStorage.setItem('player2' , JSON.stringify(player2));
+
+
+    playersInotStorage(player1 , player2)
+    
  }
  
+ document.getElementById('player_1').value = '';
+ document.getElementById('player_2').value = '';
+
+ window.location.href = '/Board.html';
 })
 
 
+
+
+
+function playersInotStorage(player1 , player2){
+
+   localStorage.setItem('player1' , JSON.stringify(player1));
+   localStorage.setItem('player2' , JSON.stringify(player2));   
+}
+
+
 // removeEventListener('submit',)
-
-
-
-
-
-
 
 
 
